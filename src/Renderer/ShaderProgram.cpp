4512488dae1d87543486aca392aceffbfd29b20d
Bogdan Yuryev <yurev.bogdan.2005@mail.ru>
2024-06-09 19:55:15 +0300

    Testing

---
+++ src/Renderer/ShaderProgram.cpp
@@ -0,0 +1,75 @@
+#include "ShaderProgram.h"
+
+#include <iostream>
+
+namespace Renderer
+{
+    ShaderProgram::ShaderProgram(const std::string &vertexShader, const std::string &fragmentShader)
+    {
+        GLuint vetrexShaderID;
+        if (!createShader(vertexShader,GL_VERTEX_SHADER, vetrexShaderID))
+        {
+            std::cerr << "VERTEX SHADER compile-time-error" << std::endl;
+            return;
+        }
+
+        GLuint fragmentShaderID;
+        if (!createShader(fragmentShader,GL_FRAGMENT_SHADER, fragmentShaderID))
+        {
+            std::cerr << "FRAGMENT SHADER compile-time-error" << std::endl;
+            glDeleteShader(vetrexShaderID);
+            return;
+        }
+
+        m_ID = glCreateProgram();
+        glAttachShader(m_ID, vetrexShaderID);
+        glAttachShader(m_ID, fragmentShaderID);
+        glLinkProgram(m_ID);
+
+        GLint success;
+        glGetProgramiv(m_ID, GL_LINK_STATUS, &success);
+        if(!success)
+        {
+            GLchar infoLog[1024];
+            glad_glGetShaderInfoLog(m_ID, 1024, nullptr, infoLog);
+            std::cerr << "ERROR::SHADER: LINK-time-error:\n"
+                      << infoLog << std::endl;
+        }
+        else
+        {
+            m_isCompiled = true;
+        }
+
+        glDeleteShader(vetrexShaderID);
+        glDeleteShader(fragmentShaderID);
+    }
+
+    ShaderProgram::~ShaderProgram()
+    {
+        glDeleteProgram(m_ID);
+    }
+
+    bool ShaderProgram::use() const
+    {
+        glUseProgram(m_ID);
+    }
+
+    bool ShaderProgram::createShader(const std::string &source, const GLenum shaderType, GLuint &shaderID)
+    {
+        shaderID = glCreateShader(shaderType);
+        const char *code = source.c_str();
+        glShaderSource(shaderID, 1, &code, nullptr);
+        glCompileShader(shaderID);
+        GLint success;
+        glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            GLchar infoLog[1024];
+            glad_glGetShaderInfoLog(shaderID, 1024, nullptr, infoLog);
+            std::cerr << "ERROR::SHADER: Compile-time-error:\n"
+                      << infoLog << std::endl;
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/Renderer/ShaderProgram.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <glad/glad.h>
+
+namespace Renderer
+{
+    class ShaderProgram
+    {
+    public:
+        ShaderProgram(const std::string &vertexShader, const std::string &fragmentShader);
+        ~ShaderProgram();
+        ShaderProgram() = delete;
+        ShaderProgram(ShaderProgram &) = delete;
+        ShaderProgram &operator=(const ShaderProgram &) = delete;
+        ShaderProgram &operator=(ShaderProgram &&shaderProgram);
+        ShaderProgram(ShaderProgram &&shaderProgram);
+        bool isCompiled() const { return m_isCompiled; }
+
+        bool use() const;
+
+    private:
+        bool m_isCompiled = false;
+        GLuint m_ID = 0;
+        bool createShader(const std::string &source, const GLenum shaderType, GLuint &shaderID);
+    };
+}
