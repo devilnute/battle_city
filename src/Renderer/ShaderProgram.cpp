adec838cc10d069a317d2a57293a6b55a42d4f58
devilnute <yurev.bogdan.2005@mail.ru>
2024-07-23 12:50:56 +0300

    Final

--- src/Renderer/ShaderProgram.cpp
+++ src/Renderer/ShaderProgram.cpp
@@ -1,28 +1,29 @@
 #include "ShaderProgram.h"
 
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 
-namespace Renderer
-{
-    ShaderProgram::ShaderProgram(const std::string &vertexShader, const std::string &fragmentShader)
+namespace RenderEngine {
+    ShaderProgram::ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader)
     {
-        GLuint vetrexShaderID;
-        if (!createShader(vertexShader, GL_VERTEX_SHADER, vetrexShaderID))
+        GLuint vertexShaderID;
+        if (!createShader(vertexShader, GL_VERTEX_SHADER, vertexShaderID))
         {
-            std::cerr << "VERTEX SHADER compile-time-error" << std::endl;
+            std::cerr << "VERTEX SHADER compile-time error" << std::endl;
             return;
         }
 
         GLuint fragmentShaderID;
         if (!createShader(fragmentShader, GL_FRAGMENT_SHADER, fragmentShaderID))
         {
-            std::cerr << "FRAGMENT SHADER compile-time-error" << std::endl;
-            glDeleteShader(vetrexShaderID);
+            std::cerr << "FRAGMENT SHADER compile-time error" << std::endl;
+            glDeleteShader(vertexShaderID);
             return;
         }
 
         m_ID = glCreateProgram();
-        glAttachShader(m_ID, vetrexShaderID);
+        glAttachShader(m_ID, vertexShaderID);
         glAttachShader(m_ID, fragmentShaderID);
         glLinkProgram(m_ID);
 
@@ -31,17 +32,36 @@ namespace Renderer
         if (!success)
         {
             GLchar infoLog[1024];
-            glad_glGetShaderInfoLog(m_ID, 1024, nullptr, infoLog);
-            std::cerr << "ERROR::SHADER: LINK-time-error:\n"
-                      << infoLog << std::endl;
+            glGetShaderInfoLog(m_ID, 1024, nullptr, infoLog);
+            std::cerr << "ERROR::SHADER: Link-time error:\n" << infoLog << std::endl;
         }
         else
         {
             m_isCompiled = true;
         }
 
-        glDeleteShader(vetrexShaderID);
+        glDeleteShader(vertexShaderID);
         glDeleteShader(fragmentShaderID);
+    }
+
+
+    bool ShaderProgram::createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID)
+    {
+        shaderID = glCreateShader(shaderType);
+        const char* code = source.c_str();
+        glShaderSource(shaderID, 1, &code, nullptr);
+        glCompileShader(shaderID);
+
+        GLint success;
+        glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            GLchar infoLog[1024];
+            glGetProgramInfoLog(shaderID, 1024, nullptr, infoLog);
+            std::cerr << "ERROR::SHADER: Compile-time error:\n" << infoLog << std::endl;
+            return false;
+        }
+        return true;
     }
 
     ShaderProgram::~ShaderProgram()
@@ -54,42 +74,38 @@ namespace Renderer
         glUseProgram(m_ID);
     }
 
-    bool ShaderProgram::createShader(const std::string &source, const GLenum shaderType, GLuint &shaderID)
-    {
-        shaderID = glCreateShader(shaderType);
-        const char *code = source.c_str();
-        glShaderSource(shaderID, 1, &code, nullptr);
-        glCompileShader(shaderID);
-        GLint success;
-        glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
-        if (!success)
-        {
-            GLchar infoLog[1024];
-            glad_glGetShaderInfoLog(shaderID, 1024, nullptr, infoLog);
-            std::cerr << "ERROR::SHADER: Compile-time-error:\n"
-                      << infoLog << std::endl;
-            return false;
-        }
-        return true;
-    }
-
-    ShaderProgram &ShaderProgram::operator=(ShaderProgram &&shaderProgram) noexcept
+    ShaderProgram& ShaderProgram::operator=(ShaderProgram&& shaderProgram) noexcept
     {
         glDeleteProgram(m_ID);
         m_ID = shaderProgram.m_ID;
         m_isCompiled = shaderProgram.m_isCompiled;
 
         shaderProgram.m_ID = 0;
-        shaderProgram.m_isCompiled = 0;
+        shaderProgram.m_isCompiled = false;
         return *this;
     }
 
-    ShaderProgram::ShaderProgram(ShaderProgram &&shaderProgram) noexcept
+    ShaderProgram::ShaderProgram(ShaderProgram&& shaderProgram) noexcept
     {
         m_ID = shaderProgram.m_ID;
         m_isCompiled = shaderProgram.m_isCompiled;
 
         shaderProgram.m_ID = 0;
-        shaderProgram.m_isCompiled = 0;
+        shaderProgram.m_isCompiled = false;
+    }
+
+    void ShaderProgram::setInt(const std::string& name, const GLint value)
+    {
+        glUniform1i(glGetUniformLocation(m_ID, name.c_str()), value);
+    }
+
+    void ShaderProgram::setFloat(const std::string& name, const GLfloat value)
+    {
+        glUniform1f(glGetUniformLocation(m_ID, name.c_str()), value);
+    }
+
+    void ShaderProgram::setMatrix4(const std::string& name, const glm::mat4& matrix)
+    {
+        glUniformMatrix4fv(glGetUniformLocation(m_ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(matrix));
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,43 @@
-#define GLFW_INCLUDE_NONE
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/vec2.hpp>
+
 #include <iostream>
+#include <chrono>
 
-#include "Renderer/ShaderProgram.h"
+#include "Game/Game.h"
 #include "Resources/ResourceManager.h"
+#include "Renderer/Renderer.h"
+#include "Physics/PhysicsEngine.h"
 
-size_t g_windowSizeX = 640;
-size_t g_windowSizeY = 480;
+static constexpr unsigned int SCALE = 3;
+static constexpr unsigned int BLOCK_SIZE = 16;
+glm::uvec2 g_windowSize(SCALE * 16 * BLOCK_SIZE, SCALE * 15 * BLOCK_SIZE);
+std::unique_ptr<Game> g_game = std::make_unique<Game>(g_windowSize);
 
-GLfloat point[] = {
-    0.0f,
-    0.5f,
-    0.0f,
-    0.5f,
-    -0.5f,
-    0.0f,
-    -0.5f,
-    -0.5f,
-    0.0f,
-};
 
-GLfloat color[] = {
-    1.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 1.0f};
-
-const char *vertex_shader =
-    "#version 460\n"
-    "layout(location = 0) in vec3 vertex_position;"
-    "layout(location = 1) in vec3 vertex_color;"
-    "out vec3 color;"
-    "void main() {"
-    "   color = vertex_color;"
-    "   gl_Position = vec4(vertex_position, 1.0);"
-    "}";
-
-const char *fragment_shader =
-    "#version 460\n"
-    "in vec3 color;"
-    "out vec4 frag_color;"
-    "void main() {"
-    "   frag_color = vec4(color,1.0);"
-    "}";
-
-void glfwWindowSizeCallback(GLFWwindow *pWindow, int width, int height)
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
 {
-    g_windowSizeX = width;
-    g_windowSizeY = height;
-    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+    g_windowSize.x = width;
+    g_windowSize.y = height;
+    g_game->setWindowSize(g_windowSize);
 }
 
-void glfwKeyCallback(GLFWwindow *pWindow, int key, int scancode, int action, int mode)
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     {
         glfwSetWindowShouldClose(pWindow, GL_TRUE);
     }
+    g_game->setKey(key, action);
 }
 
-int main(int argc, const char **argv)
+int main(int argc, char** argv)
 {
-
     /* Initialize the library */
     if (!glfwInit())
     {
-        std::cout << "glfwInit failed" << std::endl;
+        std::cout << "glfwInit failed!" << std::endl;
         return -1;
     }
 
@@ -75,13 +46,14 @@ int main(int argc, const char **argv)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow *pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "battle_city", nullptr, nullptr);
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSize.x, g_windowSize.y, "Battle City", nullptr, nullptr);
     if (!pWindow)
     {
-        std::cout << "glfwCreateWindow failed" << std::endl;
+        std::cout << "glfwCreateWindow failed!" << std::endl;
         glfwTerminate();
         return -1;
     }
+
     glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
     glfwSetKeyCallback(pWindow, glfwKeyCallback);
 
@@ -90,80 +62,48 @@ int main(int argc, const char **argv)
 
     if (!gladLoadGL())
     {
-        std::cout << "Can't load GLAD" << std::endl;
-        return -1;
+        std::cout << "Can't load GLAD!" << std::endl;
     }
 
-    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
-    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "Renderer: " << RenderEngine::Renderer::getRendererStr() << std::endl;
+    std::cout << "OpenGL version: " << RenderEngine::Renderer::getVersionStr() << std::endl;
 
-    glClearColor(0, 0, 0, 1);
+    RenderEngine::Renderer::setClearColor(0, 0, 0, 1);
+    RenderEngine::Renderer::setDepthTest(true);
+
     {
-        ResourceManager resourceManager(argv[0]);
+        ResourceManager::setExecutablePath(argv[0]);
+        Physics::PhysicsEngine::init();
+        g_game->init();
 
-        auto pDefaultShaderProgram = resourceManager.loadShaders("DefaultShader", "res/Shaders/vertex.txt", "res/Shaders/fragment.txt");
-
-        if (!pDefaultShaderProgram)
-        {
-            std::cerr << "Can't create shader program!\n";
-            return -1;
-        }
-
-        resourceManager.loadTexture("DefaultTexture", "res/Textures/map_16x16.png");
-
-        std::string vertexShader(vertex_shader);
-        std::string fragmentShader(fragment_shader);
-        Renderer::ShaderProgram shaderProgram(vertexShader, fragmentShader);
-
-        if (!shaderProgram.isCompiled())
-        {
-            std::cerr << "Can't create shader program!\n";
-            return -1;
-        }
-
-        GLuint points_vbo = 0;
-        glGenBuffers(1, &points_vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
-
-        GLuint colors_vbo = 0;
-        glGenBuffers(1, &colors_vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
-
-        GLuint vao = 0;
-        glGenVertexArrays(1, &vao);
-        glBindVertexArray(vao);
-
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        //glfwSetWindowSize(pWindow, static_cast<int>(3 * g_game->getCurrentWidth()), static_cast<int>(3 * g_game->getCurrentHeight()));
+        auto lastTime = std::chrono::high_resolution_clock::now();
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(pWindow))
         {
+            /* Poll for and process events */
+            glfwPollEvents();
+
+            auto currentTime = std::chrono::high_resolution_clock::now();
+            double duration = std::chrono::duration<double, std::milli>(currentTime - lastTime).count();
+            lastTime = currentTime;
+            g_game->update(duration);
+            Physics::PhysicsEngine::update(duration);
+
             /* Render here */
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            pDefaultShaderProgram->use();
-
-            glBindVertexArray(vao);
-
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            RenderEngine::Renderer::clear();
+            
+            g_game->render();
 
             /* Swap front and back buffers */
             glfwSwapBuffers(pWindow);
-            /* Poll for and process events */
-            glfwPollEvents();
         }
+        Physics::PhysicsEngine::terminate();
+        g_game = nullptr;
+        ResourceManager::unloadAllResources();
     }
-    
 
     glfwTerminate();
-
     return 0;
 }
